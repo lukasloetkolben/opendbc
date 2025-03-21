e55c5c2b1ff973cd77fe8c3698dcd7a4bd5138c4
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2025-03-21 11:34:38 +0100

    simplify code?

--- opendbc/car/tesla/tesla_traffic_light.py
+++ opendbc/car/tesla/tesla_traffic_light.py
@@ -125,11 +125,7 @@ class TeslaTrafficLight:
       if calculated_decel > -1.5:
         calculated_decel = np.clip(calculated_decel, a_ego - 0.07, a_ego + 0.07)
 
-      if light_status["distance"] < 6:
-        should_stop = True
-      else:
-        should_stop = False
-
+      should_stop = light_status["distance"] < 6
       pid_accel_limits = self.CI.get_pid_accel_limits(self.CP, CS.vEgo, CS.vCruise * CV.KPH_TO_MS)
       required_decel = float(self.LoC.update(CC.longActive, CS, calculated_decel, should_stop, pid_accel_limits))
 

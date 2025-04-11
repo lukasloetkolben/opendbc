45711e4082633fdb5387610a34c1d18372aeade1
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2025-04-11 17:16:18 +0200

    output_accel average

--- opendbc/car/tesla/tesla_stop.py
+++ opendbc/car/tesla/tesla_stop.py
@@ -83,13 +83,13 @@ class TeslaStop:
 
             required_decel = self.calculate_required_deceleration(v_ego, stop_line_distance, offset=-3)
             self.required_decelerations.append(required_decel)
-            self.required_decelerations = self.required_decelerations[:-max((stop_line_distance // 2), 5)]
+            self.required_decelerations = self.required_decelerations[-max((stop_line_distance // 2), 5):]
             output_accel = sum(self.required_decelerations) / len(self.required_decelerations)
 
             if self.phase == 0:
                 output_accel = clip(output_accel, self.last_accel - 0.04, self.last_accel + 0.04)
 
-            if avg_stop_line_distance < 12 and self.phase == 0:
+            if stop_line_distance < 12 and self.phase == 0:
                 self.phase = 1
 
             if self.phase == 1:

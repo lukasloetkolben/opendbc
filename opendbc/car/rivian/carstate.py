48a1291bc2bee07ff7fa98470e94a205f50032e9
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2025-04-27 21:37:27 +0200

    remove sync logic

--- opendbc/car/rivian/carstate.py
+++ opendbc/car/rivian/carstate.py
@@ -61,20 +61,20 @@ class CarState(CarStateBase):
     ret.cruiseState.enabled = cp_cam.vl["ACM_Status"]["ACM_FeatureStatus"] == 1
 
     # Button logic
-    increase_btn_pressed_now = cp_park.vl["WheelButtons"]["RightButton_RightClick"] == 1
-    decrease_btn_pressed_now = cp_park.vl["WheelButtons"]["RightButton_LeftClick"] == 1
+    increase_btn_pressed_now = cp_park.vl["WheelButtons"]["RightButton_RightClick"] == 2
+    decrease_btn_pressed_now = cp_park.vl["WheelButtons"]["RightButton_LeftClick"] == 2
 
     self.increase_cntr = self.increase_cntr + 1 if increase_btn_pressed_now else 0
     self.decrease_cntr = self.decrease_cntr + 1 if decrease_btn_pressed_now else 0
 
     # Check if increase button was pressed in the previous frame and is not pressed now (falling edge)
-    if increase_btn_pressed_now and self.increase_cntr % 500 == 0:
+    if increase_btn_pressed_now and self.increase_cntr % 50 == 0:
       self.set_speed += 5 * CV.MPH_TO_MS
     elif self.increase_btn_pressed_prev and not increase_btn_pressed_now:
       self.set_speed += 1 * CV.MPH_TO_MS
 
     # Check if decrease button was pressed in the previous frame and is not pressed now (falling edge)
-    if decrease_btn_pressed_now and self.decrease_cntr % 500 == 0:
+    if decrease_btn_pressed_now and self.decrease_cntr % 50 ^== 0:
       self.set_speed -= 5 * CV.MPH_TO_MS
     elif self.decrease_btn_pressed_prev and not decrease_btn_pressed_now:
       self.set_speed -= 1 * CV.MPH_TO_MS
